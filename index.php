fd5d7cc81799045245a81f2c621d0b4ecfb602a6
rochellesuarezuchi <uchirochelle@minsu.edu.ph>
2025-11-26 16:07:47 +0800

    m

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 if (in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: $origin");
-    header("Access-Control-Allow-Credentials: true");
+    header(header: "Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 }

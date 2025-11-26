e60e5e0dcd6876808e2d92226eecedebf3dbc6c4
rochellesuarezuchi <uchirochelle@minsu.edu.ph>
2025-11-26 16:02:32 +0800

    m

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Allow requests from local dev and Vercel frontend
 $allowedOrigins = [
     'http://localhost:5173', // local development
-    'https://frontendfinalblogflow.vercel.app', // replace with your Vercel URL
+    'https://frontend-blog-flow.vercel.app', // replace with your Vercel URL
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
